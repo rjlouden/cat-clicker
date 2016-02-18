30796f21ebc355b9c56127c33c99ab826094d606
rjlouden <private>
2016-02-17 22:12:18 -0500

    Working 2 cat website.

--- js/cats.js
+++ js/cats.js
@@ -1,39 +1,34 @@
-var cats = [{
-	"image": "images/kitten.jpg",
-	"name": "Sweet Kitty",
-	"timesClicked": 0,
-	"div": "sweet"
-	},
-	{
-	"image": "images/very-happy-kitten.jpg",
-	"name": "Happy Kitty",
-	"timesClicked": 0,
-	"div": "happy"
+var cats = {
+	"sweet": {
+		"image": "images/kitten.jpg",
+		"name": "Sweet Kitty",
+		"timesClicked": 0,
+		},
+	"happy": {
+		"image": "images/very-happy-kitten.jpg",
+		"name": "Happy Kitty",
+		"timesClicked": 0,
 	}
-];
+};
 
-var catDivHTML = '<div id="%div-name%"><img src="%image%" alt="%name%"></div>';
+var catDivHTML = '<div id="%div-name%"><H1>%name%</H1><img src="%image%" alt="%name%"></div>';
     catDivHTML = catDivHTML + '<div id = "%div-name%-times"><H1>Times Clicked= %times%</H1></div>';
-for (var i in cats){
-	var thisCat = catDivHTML.replace(/%div-name%/g,cats[i].div);
-	thisCat = thisCat.replace("%image%",cats[i].image);
-	thisCat = thisCat.replace("%name%",cats[i].name);
-	thisCat = thisCat.replace("%times%",cats[i].timesClicked);
-    $("#cats").prepend(thisCat);	
-//	console.log(thisCat);
+
+for (var catID in cats){
+	var thisCat = catDivHTML.replace(/%div-name%/g,catID);
+	thisCat = thisCat.replace("%image%",cats[catID].image);
+	thisCat = thisCat.replace(/%name%/g,cats[catID].name);
+	thisCat = thisCat.replace("%times%",cats[catID].timesClicked);
+    $("#cats").append(thisCat);
 }
 
-var timesClicked=0;
-
 $(document).ready(function(){
-	for (var i in cats){
-		var divTagName = "#"+cats[i].div;
-		$(divTagName).click(function(event){
-			console.log(event);
-			var divName = cats[i].div+"-times";
-			console.log(divName);
-			cats[i].timesClicked++;
-			$( "#"+divName).replaceWith( '<div id='+divName+'><H1>Times Clicked = '+cats[i].timesClicked+"</H1></div>" );
+	for (var catID in cats){
+		$("#"+catID).click(function(event){
+			var catID = event.currentTarget.id;
+			var divTimesName = catID+"-times";
+			cats[catID].timesClicked++;
+			$( "#"+divTimesName).replaceWith( '<div id="'+divTimesName+'"><H1>Times Clicked = '+cats[catID].timesClicked+"</H1></div>" );
     });
 	}
 });
