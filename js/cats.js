923db1fc6909fad8817ca30195d1b71ca1d379b8
rjlouden <private>
2016-02-18 21:16:46 -0500

    Works but ugly.

--- js/cats.js
+++ js/cats.js
@@ -8,6 +8,21 @@ var cats = {
 		"image": "images/very-happy-kitten.jpg",
 		"name": "Happy Kitty",
 		"timesClicked": 0,
+	},
+	"fish": {
+		"image": "images/fishbowl.jpg",
+		"name": "Fish Bowl Kitty",
+		"timesClicked": 0,
+		},
+	"lovey": {
+		"image": "images/loveyCat.jpg",
+		"name": "Hugging Kittens",
+		"timesClicked": 0,
+	},
+		"justACat": {
+		"image": "images/blackAndWhite.jpg",
+		"name": "Black and White Cat",
+		"timesClicked": 0,
 	}
 };
 
@@ -17,28 +32,48 @@ var cats = {
 var catDivHTML = '<div id="%div-name%"><H1>%name%</H1><img src="%image%" alt="%name%"></div>';
     catDivHTML = catDivHTML + '<div id = "%div-name%-times"><H1>Times Clicked= %times%</H1></div>';
 
+var menuButtonHTML ='<button id="%button-id%" type="button" class="menu-button">%name%</button>';
+
 for (var catID in cats){
 	var catHTML = catDivHTML.replace(/%div-name%/g,catID);
 	catHTML = catHTML.replace("%image%",cats[catID].image);
 	catHTML = catHTML.replace(/%name%/g,cats[catID].name);
-	catHTML = catHTML.replace("%times%",cats[catID].timesClicked);
 	cats[catID].catHTML=catHTML;
-    $("#cats").append(catHTML);
 }
+
+$("#cats").append(catHTML);
 
 $(document).ready(function(){
 	
-	var jPM = $.jPanelMenu({openPosition: '15%'});
+/*	var jPM = $.jPanelMenu({openPosition: '15%'});
     jPM.on();
-	jPM.trigger();
+	jPM.trigger();*/
 	
 	for (var catID in cats){
-		$("#"+catID).click(function(event){
-			var catID = event.currentTarget.id;
-			var divTimesName = catID+"-times";
-			cats[catID].timesClicked++;
-			$( "#"+divTimesName).replaceWith( '<div id="'+divTimesName+'"><H1>Times Clicked = '+cats[catID].timesClicked+"</H1></div>" );
-    });
+		var thisButton = menuButtonHTML.replace("%name%",cats[catID].name);
+		thisButton = thisButton.replace("%button-id%","button-"+catID);
+		$("#menu").append(thisButton);
+			
+		$("#button-"+catID).click(function(event){
+			var catID = event.currentTarget.id.replace(/^button-/,"");
+			var catHTML = cats[catID].catHTML.replace("%times%",cats[catID].timesClicked);
+			$( "#cats").replaceWith('<div id="cats" style="float: left">'+catHTML+'</div>');
+			console.log(catID);
+			
+			$("#"+catID).click(function(event){catClickEvent(event)});
+		});
+	
+		$("#button-sweet").trigger('click');
 	}
 });
+
+function catClickEvent(event){
+	var catID = event.currentTarget.id;
+	var divTimesName = catID+"-times";
+	cats[catID].timesClicked++;
+	$( "#"+divTimesName).replaceWith( '<div id="'+divTimesName+'"><H1>Times Clicked = '+cats[catID].timesClicked+"</H1></div>" );
+}
+
+
+
 
